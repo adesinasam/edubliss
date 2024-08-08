e56cbb5aecb70a6c3b8a2d0428baea092cdd3f50
Adesina Akinyemi <59088328+adesinasam@users.noreply.github.com>
2024-08-08 08:37:53 +0100

    Update program.js

--- edubliss/public/js/pages/program.js
+++ edubliss/public/js/pages/program.js
@@ -4,6 +4,26 @@ frappe.ui.form.on('Program', {
             return {
                 filters: [["Company","is_group", "=", 0]]}
         });
+        frm.fields_dict['courses'].grid.get_field('course').get_query = function(doc, cdt, cdn) {
+            return {
+                filters: {
+                    'custom_disabled': 0
+                }
+            };
+        };
     }
 
 })
+
+// frappe.ui.form.on('Program', {
+//     onload: function(frm) {
+//         // Set a query for the child table's link field
+//         frm.fields_dict['courses'].grid.get_field('course').get_query = function(doc, cdt, cdn) {
+//             return {
+//                 filters: {
+//                     'custom_disabled': 0
+//                 }
+//             };
+//         };
+//     }
+// })
